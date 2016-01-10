3aa1dd7238e23fa0165cd190e3b82e35bc70c3e3
Joseph <josephmolina3388@gmail.com>
2016-01-09 20:45:31 -0800

    First commit

--- setUp.sh
+++ setUp.sh
@@ -62,6 +62,25 @@ echo "Installing cmus"
 echo
 sudo apt-get install cmus
 
+
+echo "******************************************"
+echo "Installing Atom text editor"
+sudo add-apt-repository ppa:webupd8team/atom
+sudo apt-get update
+sudo apt-get install atom
+
+clear
+echo"*******************************************"
+echo "Installing Spotify"
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
+
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+
+sudo apt-get update
+
+sudo apt-get install spotify-client
+
+
 clear
 echo "DONE!"
 echo
